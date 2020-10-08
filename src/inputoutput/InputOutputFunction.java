c2196234070f9b53e2feb3bd377196aa8ba5288b
Jution Candra Kirana <jutionck@gmail.com>
2020-10-09 01:46:08 +0700

    InputOutput

---
+++ src/inputoutput/InputOutputFunction.java
@@ -0,0 +1,22 @@
+package inputoutput;
+
+import java.util.Scanner;
+
+public class InputOutputFunction {
+
+    //Learn Input with Scanner
+    public static void main(String[] args) {
+
+        //Inisialisasi awal penggunaan scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Program penjumlahan sederhana");
+        System.out.print("Masukan Angka pertama : ");
+        //Mengambil data yang diinput user, kemudian di masukkan melalui scanner, dengan pemanggilan Scanner.nextInt()
+        //Penggunaan fungsi scanner harus sesuai ya, kalo angka ya int(Scanner.nextInt()), kalo huruf ya string(Scanner.next())
+        int value = scanner.nextInt();
+        System.out.print("Masukan Angka kedua : ");
+        int anotherValue = scanner.nextInt();
+        int result = value + anotherValue;
+        System.out.println("Hasilnya adalah : " + result);
+    }
+}
