e3d4c517e8c4c0f1d1218a29e8245378385dbc8a
Jution Candra Kirana <jutionck@gmail.com>
2020-10-26 10:49:11 +0700

    Perulangan for, while, do while

---
+++ src/perulangan/While.java
@@ -0,0 +1,15 @@
+package perulangan;
+
+public class While {
+
+    public static void main(String[] args) {
+
+        System.out.println("Perulangan while");
+        int i = 1;
+        while (i < 10) {
+
+            System.out.println("Perulangan while ke - " + i);
+            i++;
+        }
+    }
+}
---
+++ src/perulangan/forLoop.java
@@ -0,0 +1,12 @@
+package perulangan;
+
+public class forLoop {
+
+    public static void main(String[] args) {
+
+        int i;
+        for(i = 1; i <= 5; i++) {
+            System.out.println("Ini perulangan ke - " + i);
+        }
+    }
+}
