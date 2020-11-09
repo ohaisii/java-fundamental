500ccc7c9535956a9beee193d78446206acdf2cf
Jution Candra Kirana <jutionck@gmail.com>
2020-11-09 14:52:17 +0700

    Java OOP

--- src/com/mipdevp/javafundamental/basic/vehicle/Motorcycle.java
+++
@@ -1,7 +0,0 @@
-package com.mipdevp.javafundamental.basic.vehicle;
-
-public class Motorcycle {
-    public static void jumlahBan() {
-        System.out.println("Ban motor 2");
-    }
-}
--- src/com/mipdevp/javafundamental/basic/vehicle/TheTrain.java
+++
@@ -1,7 +0,0 @@
-package com.mipdevp.javafundamental.basic.vehicle;
-
-public class TheTrain {
-    public static void jumlahBan() {
-        System.out.println("Ban kereta banyak!");
-    }
-}
