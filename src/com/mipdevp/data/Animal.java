35de6d4adc56e9046fe0b98d0252aa7087ca28b6
Jution Candra Kirana <jutionck@gmail.com>
2020-11-09 16:41:06 +0700

    Inheritance and polymorphisme

---
+++ src/com/mipdevp/data/Animal.java
@@ -0,0 +1,12 @@
+package com.mipdevp.data;
+
+public class Animal {
+
+    public Animal() {
+        System.out.println("Constructor from Animal class");
+    }
+
+    public void Eating() {
+        System.out.println("Animal was eat...");
+    }
+}
---
+++ src/com/mipdevp/data/Car.java
@@ -0,0 +1,32 @@
+package com.mipdevp.data;
+
+public class Car {
+
+    private String carName;
+    private int tier;
+    private String carType;
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
+    public void setTier(int tier) {
+        this.tier = tier;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+}
