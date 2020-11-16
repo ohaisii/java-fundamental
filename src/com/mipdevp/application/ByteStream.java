8062624bb3ffca07b523f93d8c94f055e5e7dcbf
Jution Candra Kirana <jutionck@gmail.com>
2020-11-16 16:17:14 +0700

    I/O

---
+++ src/com/mipdevp/application/ByteStream.java
@@ -0,0 +1,37 @@
+package com.mipdevp.application;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ByteStream {
+
+    public static void main(String[] args) {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            in = new FileInputStream("latihan_input.txt");
+            out = new FileOutputStream("latihan_output.txt");
+
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
